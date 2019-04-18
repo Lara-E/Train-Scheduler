d43958f34af208f4de41c55263c64a36b32b6235
Lara Eller <lareh_eller@yahoo.com>
2019-04-18 14:49:36 -0400

    I updated config

---
+++ assets/config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCfi5C6UzI12c63CTvcG0jTulTKCEfvj-o",
+    authDomain: "bootcamp-homework-ed017.firebaseapp.com",
+    databaseURL: "https://bootcamp-homework-ed017.firebaseio.com",
+    projectId: "bootcamp-homework-ed017",
+    storageBucket: "bootcamp-homework-ed017.appspot.com",
+    messagingSenderId: "760827299324"
+};
